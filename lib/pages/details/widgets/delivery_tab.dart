d5dd9fb056e4e7d2d015e78aa84dd7bce8a0fd84
Martino Yovo <martinosoyen@gmail.com>
2021-04-20 19:41:20 +0200

    DeliveryTab edited

--- lib/pages/details/widgets/delivery_tab.dart
+++ lib/pages/details/widgets/delivery_tab.dart
@@ -100,144 +100,149 @@ class _DeliveryTabState extends State<DeliveryTab> {
                   physics: BouncingScrollPhysics(),
                   itemCount: 10,
                   itemBuilder: (context, index) {
-                    return Container(
-                      margin: EdgeInsets.symmetric(
-                          vertical: getProportionateScreenWidth(7)),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Container(
-                            height: getProportionateScreenWidth(90),
-                            width: getProportionateScreenWidth(90),
-                            margin: EdgeInsets.only(
-                                right: getProportionateScreenWidth(20)),
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(
-                                  getProportionateScreenWidth(10)),
-                              color: primaryColor,
+                    return InkWell(
+                      onTap: () {
+                        showModalBottomSheet(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(getProportionateScreenWidth(20)),
+                                  topRight: Radius.circular(getProportionateScreenWidth(20)),
+                                )
                             ),
-                          ),
-                          Row(
-                            children: [
-                              Column(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    "Piezss",
-                                    style: theme.textTheme.bodyText1
-                                        .copyWith(color: Colors.black),
-                                  ),
-                                  Padding(
-                                    padding: EdgeInsets.symmetric(
-                                        vertical:
-                                            getProportionateScreenWidth(8)),
-                                    child: Text(
+                            context: context,
+                            builder: (context) => fOrderDialog(context)
+                        );
+                      },
+                      child: Container(
+                        margin: EdgeInsets.symmetric(
+                            vertical: getProportionateScreenWidth(7)),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Container(
+                              height: getProportionateScreenWidth(90),
+                              width: getProportionateScreenWidth(90),
+                              margin: EdgeInsets.only(
+                                  right: getProportionateScreenWidth(20)),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(
+                                    getProportionateScreenWidth(10)),
+                                color: primaryColor,
+                              ),
+                            ),
+                            Row(
+                              children: [
+                                Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
                                       "Piezss",
-                                      style: theme.textTheme.subtitle1.copyWith(
-                                          color: Colors.grey.shade600),
+                                      style: theme.textTheme.bodyText1
+                                          .copyWith(color: Colors.black),
                                     ),
-                                  ),
-                                  Text(
-                                    "D150",
-                                    style: theme.textTheme.bodyText1
-                                        .copyWith(color: primaryColor),
-                                  ),
-                                ],
-                              ),
-                              SizedBox(
-                                width: size.width * 0.3,
-                              ),
-                              Container(
-                                alignment: Alignment.centerRight,
-                                child: IconButton(
-                                  icon: Icon(Icons.add_circle_rounded),
-                                  color: primaryColor,
-                                  iconSize: getProportionateScreenWidth(35),
-                                  onPressed: () {
-                                    showBottomSheet(
-                                      context: context,
-                                      builder: (context) => Container(
-                                        padding: EdgeInsets.symmetric(vertical: getProportionateScreenWidth(5), horizontal: getProportionateScreenWidth(10)),
-                                        color: primaryColor,
-                                        child: Row(
-                                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                          children: [
-                                            Container(
-                                              padding: EdgeInsets.symmetric(
-                                                  horizontal:
-                                                      getProportionateScreenWidth(
-                                                          12),
-                                                  vertical:
-                                                      getProportionateScreenWidth(
-                                                          7)),
-                                              decoration: BoxDecoration(
-                                                  borderRadius:
-                                                      BorderRadius.circular(3),
-                                                  color: Colors.white
-                                                      .withOpacity(0.4)),
-                                              child: Text(
-                                                "1",
-                                                style: theme.textTheme.bodyText2
-                                                    .copyWith(
-                                                        color: Colors.white,
-                                                        fontWeight:
-                                                            FontWeight.bold),
-                                              ),
-                                            ),
-                                            InkWell(
-                                              child: Text(
-                                                "View Order",
-                                                style: theme.textTheme.bodyText2
-                                                    .copyWith(
-                                                        color: Colors.white),
-                                              ),
-                                              onTap: () {
-                                                showModalBottomSheet(
-                                                    shape: RoundedRectangleBorder(
-                                                        borderRadius: BorderRadius.only(
-                                                          topLeft: Radius.circular(getProportionateScreenWidth(20)),
-                                                          topRight: Radius.circular(getProportionateScreenWidth(20)),
-                                                        )
-                                                    ),
-                                                    context: context,
-                                                    builder: (context) => fOrderDialog(context)
-                                                );
-                                              },
-                                            ),
-                                            OutlinedButton(
-                                              /*onPressed: () {},*/
-                                              child: Text("\$150",
-                                                  style: theme
-                                                      .textTheme.bodyText2
-                                                      .copyWith(
-                                                          color: Colors.white)),
-                                              style: OutlinedButton.styleFrom(
-                                                primary: blueColor,
-                                                elevation: 0,
-                                                side: BorderSide(
-                                                    color: Colors.white,
-                                                    width: 1),
-                                                shape: RoundedRectangleBorder(
-                                                    borderRadius:
-                                                        BorderRadius.circular(
-                                                            getProportionateScreenWidth(
-                                                                3))),
-                                                padding: EdgeInsets.symmetric(
-                                                    vertical: 0),
-                                              ),
-                                            ),
-                                          ],
-                                        ),
+                                    Padding(
+                                      padding: EdgeInsets.symmetric(
+                                          vertical:
+                                              getProportionateScreenWidth(8)),
+                                      child: Text(
+                                        "Piezss",
+                                        style: theme.textTheme.subtitle1.copyWith(
+                                            color: Colors.grey.shade600),
                                       ),
-                                    );
-                                  },
+                                    ),
+                                    Text(
+                                      "D150",
+                                      style: theme.textTheme.bodyText1
+                                          .copyWith(color: primaryColor),
+                                    ),
+                                  ],
                                 ),
-                              )
-                            ],
-                          )
-                        ],
+                                SizedBox(
+                                  width: size.width * 0.3,
+                                ),
+                                Container(
+                                  alignment: Alignment.centerRight,
+                                  child: IconButton(
+                                    icon: Icon(Icons.add_circle_rounded),
+                                    color: primaryColor,
+                                    iconSize: getProportionateScreenWidth(35),
+                                    onPressed: () {
+                                      showBottomSheet(
+                                        context: context,
+                                        builder: (context) => Container(
+                                          padding: EdgeInsets.symmetric(vertical: getProportionateScreenWidth(5), horizontal: getProportionateScreenWidth(10)),
+                                          color: primaryColor,
+                                          child: Row(
+                                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                            children: [
+                                              Container(
+                                                padding: EdgeInsets.symmetric(
+                                                    horizontal:
+                                                        getProportionateScreenWidth(
+                                                            12),
+                                                    vertical:
+                                                        getProportionateScreenWidth(
+                                                            7)),
+                                                decoration: BoxDecoration(
+                                                    borderRadius:
+                                                        BorderRadius.circular(3),
+                                                    color: Colors.white
+                                                        .withOpacity(0.4)),
+                                                child: Text(
+                                                  "1",
+                                                  style: theme.textTheme.bodyText2
+                                                      .copyWith(
+                                                          color: Colors.white,
+                                                          fontWeight:
+                                                              FontWeight.bold),
+                                                ),
+                                              ),
+                                              InkWell(
+                                                child: Text(
+                                                  "View Order",
+                                                  style: theme.textTheme.bodyText2
+                                                      .copyWith(
+                                                          color: Colors.white),
+                                                ),
+                                                onTap: () {
+                                                  Navigator.pushNamed(context, "/detailsMyOrder");
+                                                },
+                                              ),
+                                              OutlinedButton(
+                                                /*onPressed: () {},*/
+                                                child: Text("\$150",
+                                                    style: theme
+                                                        .textTheme.bodyText2
+                                                        .copyWith(
+                                                            color: Colors.white)),
+                                                style: OutlinedButton.styleFrom(
+                                                  primary: blueColor,
+                                                  elevation: 0,
+                                                  side: BorderSide(
+                                                      color: Colors.white,
+                                                      width: 1),
+                                                  shape: RoundedRectangleBorder(
+                                                      borderRadius:
+                                                          BorderRadius.circular(
+                                                              getProportionateScreenWidth(
+                                                                  3))),
+                                                  padding: EdgeInsets.symmetric(
+                                                      vertical: 0),
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                      );
+                                    },
+                                  ),
+                                )
+                              ],
+                            )
+                          ],
+                        ),
                       ),
                     );
                   }),
