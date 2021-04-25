466527a8151463e745daff9e0b93276581fba6d3
Martino Yovo <martinosoyen@gmail.com>
2021-04-25 10:05:49 +0200

    Food mobile shimmer loader created

---
+++ lib/widgets/f_loader.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
+
+Widget FLoader({double height, double width, double radius}) {
+  return Shimmer.fromColors(
+    baseColor: Colors.grey.shade200,
+    highlightColor: Colors.grey.shade300,
+    child: Container(
+      height: height,
+      width: width,
+      decoration: BoxDecoration(
+        color: Colors.grey.shade300,
+        borderRadius: BorderRadius.circular(radius),)
+    ),
+  );
+}
