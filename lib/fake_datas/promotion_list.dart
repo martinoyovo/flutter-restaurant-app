c74465a29f640aa329defda912f91fa45962bc8b
Martino Yovo <martinosoyen@gmail.com>
2021-04-26 12:11:35 +0200

    Promotion fake data created

---
+++ lib/fake_datas/promotion_list.dart
@@ -0,0 +1,14 @@
+List<Map<String, dynamic>> promotionList = [
+  {
+    "url": "https://images.unsplash.com/photo-1616606103895-e8fdf125c210?crop=entropy&cs=srgb&fm=jpg&ixid=MnwyMjU3NDh8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MTkyNTM1NDA&ixlib=rb-1.2.1&q=85",
+    "percentage": "10% of all food",
+    "delay": "18/3 - 22/3",
+    "isHightlighted": false
+  },
+  {
+    "url": "https://images.unsplash.com/photo-1616606103895-e8fdf125c210?crop=entropy&cs=srgb&fm=jpg&ixid=MnwyMjU3NDh8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MTkyNTM1NDA&ixlib=rb-1.2.1&q=85",
+    "percentage": "20% of all food",
+    "delay": "30/4 - 01/6",
+    "isHightlighted": true
+  },
+];
