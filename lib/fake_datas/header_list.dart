0475d61d382239eb85ea99e91d38fcfc6eddeaf2
Martino Yovo <martinosoyen@gmail.com>
2021-04-26 12:17:13 +0200

    Header list fake data edited

--- lib/fake_datas/header_list.dart
+++ lib/fake_datas/header_list.dart
@@ -1,26 +1,32 @@
-List<Map<String, String>> headerList = [
+List<Map<String, dynamic>> headerList = [
   {
-    "image": "https://images.unsplash.com/photo-1616606103895-e8fdf125c210?crop=entropy&cs=srgb&fm=jpg&ixid=MnwyMjU3NDh8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MTkyNTM1NDA&ixlib=rb-1.2.1&q=85",
-    "titre": "A&E Lunch",
-    "soutitre": "189 Avenue Street Edinbugh",
-    "review": "4.0 (230)",
-    "time": "15-25 mins",
+    "url": "https://images.unsplash.com/photo-1616606103895-e8fdf125c210?crop=entropy&cs=srgb&fm=jpg&ixid=MnwyMjU3NDh8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MTkyNTM1NDA&ixlib=rb-1.2.1&q=85",
+    "title": "A&E Lunch",
+    "rate": "4.0",
+    "number": 230,
+    "address": "189 Avenue Street Edinbugh",
+    "time": "15-25",
+    "distance": 250,
     "status": "Busy"
   },
   {
-    "image": "https://images.unsplash.com/photo-1617380951201-c3c15ad20534?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMjU3NDh8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MTkyNTM3NzQ&ixlib=rb-1.2.1&q=80&w=1080",
-    "titre": "S&L Diner",
-    "soutitre": "189 Avenue Street Edinbugh",
-    "review": "4.0 (230)",
-    "time": "15-25 mins",
+    "url": "https://images.unsplash.com/photo-1617380951201-c3c15ad20534?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMjU3NDh8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MTkyNTM3NzQ&ixlib=rb-1.2.1&q=80&w=1080",
+    "title": "Mack Donald",
+    "rate": 4.5,
+    "number": 230,
+    "address": "New York",
+    "time": 15,
+    "distance": 250,
     "status": "Free"
   },
   {
-    "image": "https://api.unsplash.com/photos/random/?client_id=GUTzjj-72-y3SOzR6Yekp1IY5x2S25_mq0fN9gKYfJ8",
-    "titre": "S&L Diner",
-    "soutitre": "189 Avenue Street Edinbugh",
-    "review": "4.0 (230)",
-    "time": "15-25 mins",
-    "status": "Free"
+    "url": "https://images.unsplash.com/photo-1617380951201-c3c15ad20534?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMjU3NDh8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MTkyNTM3NzQ&ixlib=rb-1.2.1&q=80&w=1080",
+    "title": "Elite Burger",
+    "rate": 2.7,
+    "number": 100,
+    "address": "Lome",
+    "time": 5,
+    "distance": 50,
+    "status": "Availabe"
   },
 ];
