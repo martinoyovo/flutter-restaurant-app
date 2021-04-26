734b8d4d3bac63d08117c0dd16b24c37b9d26315
Martino Yovo <martinosoyen@gmail.com>
2021-04-26 12:16:24 +0200

    Input decoration widget edited

--- lib/core/utils/f_input_decoration.dart
+++ lib/core/utils/f_input_decoration.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:food_mobile/core/utils/size_config.dart';
 
-InputDecoration fInputDecoration(text, theme) {
+InputDecoration fInputDecoration(text, theme, suffix) {
   return InputDecoration(
     labelText: text,
     hintText: text,
+    errorStyle: theme.inputDecorationTheme.errorStyle,
     labelStyle: theme.inputDecorationTheme.labelStyle,
     contentPadding: EdgeInsets.all(getProportionateScreenWidth(10)),
     border: OutlineInputBorder(borderRadius: BorderRadius.circular(getProportionateScreenWidth(15))),
+    suffixIcon: suffix?? Container()
   );
 }
