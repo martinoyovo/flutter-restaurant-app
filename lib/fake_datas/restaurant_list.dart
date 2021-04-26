c42db92e235d5c5882a4ef8c2689b8e944861477
Martino Yovo <martinosoyen@gmail.com>
2021-04-26 12:12:06 +0200

    Restaurant fake data created

---
+++ lib/fake_datas/restaurant_list.dart
@@ -0,0 +1,33 @@
+List<Map<String, dynamic>> restaurantList = [
+  {},
+  {
+    "url": "https://images.unsplash.com/photo-1616606103895-e8fdf125c210?crop=entropy&cs=srgb&fm=jpg&ixid=MnwyMjU3NDh8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MTkyNTM1NDA&ixlib=rb-1.2.1&q=85",
+    "title": "Ding tea",
+    "rate": 4.0,
+    "number": 230,
+    "address": "189 Giang Vo",
+    "time": 5,
+    "distance": 250,
+    "status": ""
+  },
+  {
+    "url": "https://images.unsplash.com/photo-1619222232983-e7fa9e740068?crop=entropy&cs=srgb&fm=jpg&ixid=MnwyMjU3NDh8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MTkzNDc0OTc&ixlib=rb-1.2.1&q=85",
+    "title": "Chocolate",
+    "rate": 3.5,
+    "number": 400,
+    "address": "Avenue Street",
+    "time": 10,
+    "distance": 200,
+    "status": ""
+  },
+  {
+    "url": "https://images.unsplash.com/photo-1618271972198-d699d4a94447?crop=entropy&cs=srgb&fm=jpg&ixid=MnwyMjU3NDh8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MTkzNDk3MDU&ixlib=rb-1.2.1&q=85",
+    "title": "King plate",
+    "rate": 5,
+    "number": 700,
+    "address": "Los Angeles",
+    "time": 50,
+    "distance": 500,
+    "status": ""
+  },
+];
