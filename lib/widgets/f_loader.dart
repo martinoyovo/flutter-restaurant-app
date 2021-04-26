7b3c692da619002b7dec95ab3c9c357912f6ec0d
Martino Yovo <martinosoyen@gmail.com>
2021-04-26 12:18:26 +0200

    Loader widget created

--- lib/widgets/f_loader.dart
+++ lib/widgets/f_loader.dart
@@ -4,7 +4,7 @@ import 'package:shimmer/shimmer.dart';
 Widget FLoader({double height, double width, double radius}) {
   return Shimmer.fromColors(
     baseColor: Colors.grey.shade200,
-    highlightColor: Colors.grey.shade300,
+    highlightColor: Colors.white30,
     child: Container(
       height: height,
       width: width,
