3056c1f956a8822fd36b94afea83c3c434ef37df
Martino Yovo <martinosoyen@gmail.com>
2021-05-01 08:32:55 +0200

    MenuList add

---
+++ lib/fake_datas/menu_list.dart
@@ -0,0 +1,102 @@
+List<Map<String, dynamic>> menuList = [
+  {
+    "name":"Betty Crocker",
+    "description":"Heinz",
+    "price":"574213758.449653"
+  },
+  {
+    "name":"Kits",
+    "description":"Lavazza",
+    "price":"-164588862.521564"
+  },
+  {
+    "name":"Soprole",
+    "description":"Domino",
+    "price":"-436988705.374637"
+  },
+  {
+    "name":"M&M Food Market",
+    "description":"Papadopoulos",
+    "price":"462542634.846014"
+  },
+  {
+    "name":"McDonald's",
+    "description":"Bewley's",
+    "price":"121248199.964051"
+  },
+  {
+    "name":"Betty Crocker",
+    "description":"Starbucks",
+    "price":"786694635.918332"
+  },
+  {
+    "name":"Soprole",
+    "description":"Soprole",
+    "price":"301978245.812394"
+  },
+  {
+    "name":"Kits",
+    "description":"Tic Tac",
+    "price":"847869092.176512"
+  },
+  {
+    "name":"Bel Group",
+    "description":"Bewley's",
+    "price":"-606445088.068584"
+  },
+  {
+    "name":"Carib Brewery",
+    "description":"Andy Capp's fries",
+    "price":"60687141.8446778"
+  },
+  {
+    "name":"McDonald's",
+    "description":"Papadopoulos",
+    "price":"-29014901.3150963"
+  },
+  {
+    "name":"Starbucks",
+    "description":"Applebee's",
+    "price":"-429389822.390474"
+  },
+  {
+    "name":"McDonald's",
+    "description":"Baskin robbins",
+    "price":"-80848354.7401253"
+  },
+  {
+    "name":"Vitta Foods",
+    "description":"Bel Group",
+    "price":"-913315419.114259"
+  },
+  {
+    "name":"Starbucks",
+    "description":"Tic Tac",
+    "price":"436748998.361259"
+  },
+  {
+    "name":"Doritos",
+    "description":"M&M Food Market",
+    "price":"-162371801.160663"
+  },
+  {
+    "name":"Taco Bell",
+    "description":"Nesquik",
+    "price":"-256240078.14842"
+  },
+  {
+    "name":"Cobblestone",
+    "description":"Heinz",
+    "price":"511775164.818376"
+  },
+  {
+    "name":"Red Lobster",
+    "description":"Soprole",
+    "price":"-69672897.7565371"
+  },
+  {
+    "name":"Wonder Bread",
+    "description":"Red Lobster",
+    "price":"-931268732.247438"
+  }
+];
