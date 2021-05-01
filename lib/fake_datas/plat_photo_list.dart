dfa0101629272001b04953123b463c4705c45ffd
Martino Yovo <martinosoyen@gmail.com>
2021-05-01 08:34:18 +0200

    PlatPhotoList widget added

---
+++ lib/fake_datas/plat_photo_list.dart
@@ -0,0 +1,17 @@
+List<Map<String, dynamic>> platPhotosList = [
+  {
+    "url": "https://images.unsplash.com/photo-1617977264618-dc6c4a7b96a6?ixid=MnwyMjU3NDh8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MTk2OTA2ODQ&ixlib=rb-1.2.1"
+  },
+  {
+    "url": "https://images.unsplash.com/photo-1618276114967-c3af49bec846?ixid=MnwyMjU3NDh8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MTk3MDEwMTE&ixlib=rb-1.2.1"
+  },
+  {
+    "url": "https://images.unsplash.com/photo-1617977264618-dc6c4a7b96a6?ixid=MnwyMjU3NDh8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MTk2OTA2ODQ&ixlib=rb-1.2.1"
+  },
+  {
+    "url": "https://images.unsplash.com/photo-1618276114967-c3af49bec846?ixid=MnwyMjU3NDh8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MTk3MDEwMTE&ixlib=rb-1.2.1"
+  },
+  {
+    "url": "https://images.unsplash.com/photo-1617977264618-dc6c4a7b96a6?ixid=MnwyMjU3NDh8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MTk2OTA2ODQ&ixlib=rb-1.2.1"
+  },
+];
