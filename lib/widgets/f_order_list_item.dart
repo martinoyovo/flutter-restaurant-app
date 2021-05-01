7265ae1fd2fc171663391679dcd5088e70de834e
Martino Yovo <martinosoyen@gmail.com>
2021-05-01 08:39:23 +0200

    lib/widgets/f_order_list_item.dart added

---
+++ lib/widgets/f_order_list_item.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:food_mobile/core/utils/colors.dart';
+import 'package:food_mobile/core/utils/size_config.dart';
+import 'package:food_mobile/fake_datas/models/restaurant_menu.dart';
+import 'package:food_mobile/widgets/f_network_image.dart';
+import 'package:google_fonts/google_fonts.dart';
+
