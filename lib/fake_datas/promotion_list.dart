f70b4b805f4675dbd0ee116cdc39ddef6acf8bf1
Martino Yovo <martinosoyen@gmail.com>
2021-05-01 08:24:33 +0200

    promotion list fake datas edited

--- lib/fake_datas/promotion_list.dart
+++ lib/fake_datas/promotion_list.dart
@@ -1,14 +1,20 @@
 List<Map<String, dynamic>> promotionList = [
   {
-    "url": "https://images.unsplash.com/photo-1616606103895-e8fdf125c210?crop=entropy&cs=srgb&fm=jpg&ixid=MnwyMjU3NDh8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MTkyNTM1NDA&ixlib=rb-1.2.1&q=85",
+    "url": "https://images.unsplash.com/photo-1618654608146-b78695ac6248?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMjU3NDh8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MTk3NzIzNTE&ixlib=rb-1.2.1&q=80&w=400",
     "percentage": "10% of all food",
     "delay": "18/3 - 22/3",
     "isHightlighted": false
   },
   {
-    "url": "https://images.unsplash.com/photo-1616606103895-e8fdf125c210?crop=entropy&cs=srgb&fm=jpg&ixid=MnwyMjU3NDh8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MTkyNTM1NDA&ixlib=rb-1.2.1&q=85",
+    "url": "https://images.unsplash.com/photo-1617657515593-22cf90b5ef71?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMjU3NDh8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MTk3NzIzMDA&ixlib=rb-1.2.1&q=80&w=400",
     "percentage": "20% of all food",
     "delay": "30/4 - 01/6",
     "isHightlighted": true
+  },
+  {
+    "url": "https://images.unsplash.com/photo-1619222232983-e7fa9e740068?crop=entropy&cs=srgb&fm=jpg&ixid=MnwyMjU3NDh8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MTkzNDc0OTc&ixlib=rb-1.2.1&q=80&w=400",
+    "percentage": "5% of all Pizza",
+    "delay": "3/4 - 21/6",
+    "isHightlighted": false
   },
 ];
