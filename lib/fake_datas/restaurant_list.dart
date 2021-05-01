70248325892aac2b11602a9fc53f80d72e9a72fc
Martino Yovo <martinosoyen@gmail.com>
2021-05-01 08:25:07 +0200

    restaurant list fake datas edited

--- lib/fake_datas/restaurant_list.dart
+++ lib/fake_datas/restaurant_list.dart
@@ -1,7 +1,7 @@
 List<Map<String, dynamic>> restaurantList = [
   {},
   {
-    "url": "https://images.unsplash.com/photo-1616606103895-e8fdf125c210?crop=entropy&cs=srgb&fm=jpg&ixid=MnwyMjU3NDh8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MTkyNTM1NDA&ixlib=rb-1.2.1&q=85",
+    "url": "https://images.unsplash.com/photo-1617393992486-fa01f9ae0524?crop=entropy&cs=srgb&fm=jpg&ixid=MnwyMjU3NDh8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MTk3NzE5MDU&ixlib=rb-1.2.1&q=80&w=400",
     "title": "Ding tea",
     "rate": 4.0,
     "number": 230,
@@ -11,7 +11,7 @@ List<Map<String, dynamic>> restaurantList = [
     "status": ""
   },
   {
-    "url": "https://images.unsplash.com/photo-1619222232983-e7fa9e740068?crop=entropy&cs=srgb&fm=jpg&ixid=MnwyMjU3NDh8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MTkzNDc0OTc&ixlib=rb-1.2.1&q=85",
+    "url": "https://images.unsplash.com/photo-1619222232983-e7fa9e740068?crop=entropy&cs=srgb&fm=jpg&ixid=MnwyMjU3NDh8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MTkzNDc0OTc&ixlib=rb-1.2.1&q=80&w=400",
     "title": "Chocolate",
     "rate": 3.5,
     "number": 400,
@@ -21,7 +21,7 @@ List<Map<String, dynamic>> restaurantList = [
     "status": ""
   },
   {
-    "url": "https://images.unsplash.com/photo-1618271972198-d699d4a94447?crop=entropy&cs=srgb&fm=jpg&ixid=MnwyMjU3NDh8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MTkzNDk3MDU&ixlib=rb-1.2.1&q=85",
+    "url": "https://images.unsplash.com/photo-1618503887680-9a353f19a023?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMjU3NDh8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MTk3NzE5OTc&ixlib=rb-1.2.1&q=80&w=400",
     "title": "King plate",
     "rate": 5,
     "number": 700,
