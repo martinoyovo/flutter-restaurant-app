3327c314822f227897d7a1f94a03ef162393fbf6
Martino Yovo <martinosoyen@gmail.com>
2021-05-01 08:44:35 +0200

    promotion list edited

---
+++ lib/fake_datas/promotion_list.dart
@@ -0,0 +1,32 @@
+List<Map<String, dynamic>> promotionList = [
+  {
+    "url": "https://images.unsplash.com/photo-1617393992486-fa01f9ae0524?crop=entropy&cs=srgb&fm=jpg&ixid=MnwyMjU3NDh8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MTk3NzE5MDU&ixlib=rb-1.2.1&q=80&w=400",
+    "title": "Ding tea",
+    "rate": 4.0,
+    "number": 230,
+    "address": "189 Giang Vo",
+    "time": 5,
+    "distance": 250,
+    "status": ""
+  },
+  {
+    "url": "https://images.unsplash.com/photo-1619222232983-e7fa9e740068?crop=entropy&cs=srgb&fm=jpg&ixid=MnwyMjU3NDh8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MTkzNDc0OTc&ixlib=rb-1.2.1&q=80&w=400",
+    "title": "Chocolate",
+    "rate": 3.5,
+    "number": 400,
+    "address": "Avenue Street",
+    "time": 10,
+    "distance": 200,
+    "status": ""
+  },
+  {
+    "url": "https://images.unsplash.com/photo-1618503887680-9a353f19a023?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMjU3NDh8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MTk3NzE5OTc&ixlib=rb-1.2.1&q=80&w=400",
+    "title": "King plate",
+    "rate": 5,
+    "number": 700,
+    "address": "Los Angeles",
+    "time": 50,
+    "distance": 500,
+    "status": ""
+  },
+];
