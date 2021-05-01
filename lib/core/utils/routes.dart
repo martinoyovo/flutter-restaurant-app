9a162c096b4ad13440c27512209caf71729268b7
Martino Yovo <martinosoyen@gmail.com>
2021-05-01 08:32:31 +0200

    Routes added into new file

---
+++ lib/core/utils/routes.dart
@@ -0,0 +1,75 @@
+import 'package:food_mobile/pages/address/address_edit.dart';
+import 'package:food_mobile/pages/address/address_edit2.dart';
+import 'package:food_mobile/pages/address/location_address.dart';
+import 'package:food_mobile/pages/address/location_choose_city.dart';
+import 'package:food_mobile/pages/auth/confirm_password.dart';
+import 'package:food_mobile/pages/auth/sign_in.dart';
+import 'package:food_mobile/pages/auth/sign_up.dart';
+import 'file:///C:/Users/tino.co/Documents/food_mobile/lib/pages/details/choose_dessert.dart';
+import 'package:food_mobile/pages/details/details_add_promotion_code.dart';
+import 'package:food_mobile/pages/details/details_change_payment.dart';
+import 'package:food_mobile/pages/details/details_infos.dart';
+import 'package:food_mobile/pages/details/details_my_order.dart';
+import 'package:food_mobile/pages/details/details_my_order2.dart';
+import 'package:food_mobile/pages/details/details_search_in_restaurant.dart';
+import 'package:food_mobile/pages/driver/order/message/order_message.dart';
+import 'package:food_mobile/pages/driver/order/order_details.dart';
+import 'package:food_mobile/pages/driver/order/track_order.dart';
+import 'package:food_mobile/pages/driver/order/tracking.dart';
+import 'file:///C:/Users/tino.co/Documents/food_mobile/lib/widgets/f_bottom_navigation.dart';
+import 'package:food_mobile/pages/home.dart';
+import 'package:food_mobile/pages/my_order/view_order.dart';
+import 'package:food_mobile/pages/onboarding.dart';
+import 'package:food_mobile/pages/profile/edit_profile.dart';
+import 'package:food_mobile/pages/profile/profile_screen.dart';
+import 'package:food_mobile/pages/saved/see_all.dart';
+import 'package:food_mobile/pages/search/search_in_restaurant.dart';
+import 'package:food_mobile/root.dart';
+import 'package:food_mobile/pages/search/search.dart';
+import 'package:food_mobile/pages/search/search_filter.dart';
+import 'package:food_mobile/pages/search/search_items.dart';
+
+var fRoutes = {
+  "/home": (context) => Home(),
+  "/root": (context) => Root(),
+
+  '/': (context) => Onboarding(),
+  "/signIn": (context) => SignIn(),
+  "/signUp": (context) => SignUp(),
+  "/confirmPassword": (context) => ConfirmPassword(),
+
+  "/chooseDessert": (context) => ChooseDessert(),
+  "/searchInRestaurant": (context) => SearchInRestaurant(),
+  "/detailsSearchInRestaurant": (context) => DetailsSearchInRestaurant(),
+
+  "/searchItems": (context) => SearchItems(),
+  "/search": (context) => Search(),
+  "/searchFilter": (context) => SearchFilter(),
+
+  "/locationChooseCity": (context) => LocationChooseCity(),
+  "/locationAddress": (context) => LocationAddress(),
+  "/editAddress": (context) => AddressEdit(),
+  "/editAddress2": (context) => AddressEdit2(),
+
+  "/detailsInfos": (context) => DetailsInfos(),
+  "/detailsMyOrder": (context) => DetailsMyOrder(),
+  "/detailsMyOrder2": (context) => DetailsMyOrder2(),
+  "/detailsChangePayment": (context) => DetailsChangePayment(),
+  "/detailsAddPromotionCode": (context) => DetailsAddPromotionCode(),
+
+  "/orderMessage": (context) => OrderMessage(),
+  "/orderDetails": (context) => OrderDetails(),
+  "/trackOrder": (context) => TrackOrder(),
+  "/tracking": (context) => Tracking(),
+  "/driver": (context) => FBottomNavigationBar(),
+
+  "/seeAll": (context) => SeeAll(),
+
+  "/profileScreen": (context) => ProfileScreen(),
+  "/editProfile": (context) => EditProfile(),
+
+  "/nav": (context) => FBottomNavigationBar(),
+
+  //ORDER
+  "/viewOrder": (context) => ViewOrder(),
+};
