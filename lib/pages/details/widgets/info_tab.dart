d7cea4eefa6f06a50aacfca088bd0165c25397dd
Martino Yovo <martinosoyen@gmail.com>
2021-05-01 08:28:23 +0200

    Infos tab edited

--- lib/pages/details/widgets/info_tab.dart
+++ lib/pages/details/widgets/info_tab.dart
@@ -6,55 +6,47 @@ class InfoTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    return Container(
-      margin: EdgeInsets.fromLTRB(
-        getProportionateScreenWidth(20),
-        getProportionateScreenWidth(0),
-        getProportionateScreenWidth(20),
-        getProportionateScreenWidth(20),
-      ),
-      child: ListView(
-        physics: BouncingScrollPhysics(),
-        children: [
-          Container(
-            margin: EdgeInsets.only(bottom: getProportionateScreenWidth(10)),
-            height: getProportionateScreenWidth(110),
-            width: double.infinity,
-            decoration: BoxDecoration(
-                borderRadius:
-                    BorderRadius.circular(getProportionateScreenWidth(20)),
-                color: primaryColor),
+    return ListView(
+      physics: BouncingScrollPhysics(),
+      children: [
+        Container(
+          margin: EdgeInsets.only(bottom: getProportionateScreenWidth(10)),
+          height: getProportionateScreenWidth(110),
+          width: double.infinity,
+          decoration: BoxDecoration(
+              borderRadius:
+                  BorderRadius.circular(getProportionateScreenWidth(20)),
+              color: primaryColor),
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 4.0),
+          child: Text(
+            "American Restaurant - Diner",
+            style: theme.textTheme.bodyText1.copyWith(color: Colors.black),
           ),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 4.0),
-            child: Text(
-              "American Restaurant - Diner",
-              style: theme.textTheme.bodyText1.copyWith(color: Colors.black),
-            ),
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 4.0),
+          child: Text(
+            "Address: 189 Avenue Street",
+            style: theme.textTheme.bodyText1.copyWith(color: Colors.black),
           ),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 4.0),
-            child: Text(
-              "Address: 189 Avenue Street",
-              style: theme.textTheme.bodyText1.copyWith(color: Colors.black),
-            ),
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 4.0),
+          child: Text(
+            "Phone: 0123.456.789",
+            style: theme.textTheme.bodyText1.copyWith(color: Colors.black),
           ),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 4.0),
-            child: Text(
-              "Phone: 0123.456.789",
-              style: theme.textTheme.bodyText1.copyWith(color: Colors.black),
-            ),
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 4.0),
+          child: Text(
+            "Working hours: Daily from 10AM - 10.30PM",
+            style: theme.textTheme.bodyText1.copyWith(color: Colors.black),
           ),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 4.0),
-            child: Text(
-              "Working hours: Daily from 10AM - 10.30PM",
-              style: theme.textTheme.bodyText1.copyWith(color: Colors.black),
-            ),
-          ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
