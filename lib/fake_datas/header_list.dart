dba8f1c9e43a46aa8b7b2eeb29e996f2d4cbb918
Martino Yovo <martinosoyen@gmail.com>
2021-05-01 08:23:52 +0200

    header list fake datas edited

--- lib/fake_datas/header_list.dart
+++ lib/fake_datas/header_list.dart
@@ -1,6 +1,6 @@
 List<Map<String, dynamic>> headerList = [
   {
-    "url": "https://images.unsplash.com/photo-1616606103895-e8fdf125c210?crop=entropy&cs=srgb&fm=jpg&ixid=MnwyMjU3NDh8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MTkyNTM1NDA&ixlib=rb-1.2.1&q=85",
+    "url": "https://images.unsplash.com/photo-1616606103895-e8fdf125c210?crop=entropy&cs=srgb&fm=jpg&ixid=MnwyMjU3NDh8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MTkyNTM1NDA&ixlib=rb-1.2.1&q=80&w=400",
     "title": "A&E Lunch",
     "rate": "4.0",
     "number": 230,
@@ -10,7 +10,7 @@ List<Map<String, dynamic>> headerList = [
     "status": "Busy"
   },
   {
-    "url": "https://images.unsplash.com/photo-1617380951201-c3c15ad20534?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMjU3NDh8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MTkyNTM3NzQ&ixlib=rb-1.2.1&q=80&w=1080",
+    "url": "https://images.unsplash.com/photo-1617380951201-c3c15ad20534?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMjU3NDh8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MTkyNTM3NzQ&ixlib=rb-1.2.1&&q=80&w=400",
     "title": "Mack Donald",
     "rate": 4.5,
     "number": 230,
@@ -20,7 +20,7 @@ List<Map<String, dynamic>> headerList = [
     "status": "Free"
   },
   {
-    "url": "https://images.unsplash.com/photo-1617380951201-c3c15ad20534?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMjU3NDh8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MTkyNTM3NzQ&ixlib=rb-1.2.1&q=80&w=1080",
+    "url": "https://images.unsplash.com/photo-1617380951201-c3c15ad20534?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMjU3NDh8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MTkyNTM3NzQ&ixlib=rb-1.2.1&&q=80&w=400",
     "title": "Elite Burger",
     "rate": 2.7,
     "number": 100,
