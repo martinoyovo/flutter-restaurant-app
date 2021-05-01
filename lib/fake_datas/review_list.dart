a79633196c080ef9c1914d30c4ce91c1ae157c84
Martino Yovo <martinosoyen@gmail.com>
2021-05-01 08:34:45 +0200

    ReviewList added

---
+++ lib/fake_datas/review_list.dart
@@ -0,0 +1,52 @@
+List<Map<String, dynamic>> reviewList = [
+  {
+    "date":"Saturday, October 26, 8452",
+    "text":"Nurofen",
+    "fullName":"Marjorie Douglas"
+  },
+  {
+    "date":"Friday, October 9, 9592",
+    "text":"Singulair",
+    "fullName":"Kendra Yarlett"
+  },
+  {
+    "date":"Sunday, October 9, 8918",
+    "text":"Arcoxia",
+    "fullName":"Liam Jarvis"
+  },
+  {
+    "date":"Saturday, September 19, 7170",
+    "text":"Exact",
+    "fullName":"Tess Irving"
+  },
+  {
+    "date":"Sunday, November 8, 7254",
+    "text":"Zetril",
+    "fullName":"Winnie Davies"
+  },
+  {
+    "date":"Saturday, August 13, 8236",
+    "text":"Aspirin",
+    "fullName":"Julia Jenkins"
+  },
+  {
+    "date":"Saturday, May 11, 1224",
+    "text":"Tylenol",
+    "fullName":"Margot Larkin"
+  },
+  {
+    "date":"Friday, April 20, 2981",
+    "text":"MidNite",
+    "fullName":"Caleb Bloom"
+  },
+  {
+    "date":"Wednesday, October 14, 2781",
+    "text":"Singulair",
+    "fullName":"Naomi Allington"
+  },
+  {
+    "date":"Friday, August 1, 7259",
+    "text":"Paracetamol",
+    "fullName":"Mina Gray"
+  }
+];
